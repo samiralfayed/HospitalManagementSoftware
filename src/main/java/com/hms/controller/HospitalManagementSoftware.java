a59a37bde1c9fac0d1494dd400b57585c618515e
samiralfayed <samiralfayedifti001@gmail.com>
2024-08-14 17:29:28 +0600

    It is a Hospital Management Software
    Here we work with pathology and internal system.

---
+++ src/main/java/com/hms/controller/HospitalManagementSoftware.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.hms.controller;
+
+import com.hms.view.Home;
+
+/**
+ *
+ * @author SIMEC Institute
+ */
+public class HospitalManagementSoftware {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+         new Home().setVisible(true);
+    }
+}
---
+++ src/main/java/com/hms/model/PathologicalTest.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hms.model;
+
+/**
+ *
+ * @author SIMEC Institute
+ */
+public class PathologicalTest {
+    
+    String title;
+    double cost;
+    boolean isAvailable;
+    
+    
+    PathologicalTest( String title,
+                       double cost,
+                       boolean isAvailable){
+        this.title = title;
+        this.cost = cost;
+        this.isAvailable = isAvailable;
+    }
+    
+    String show(){
+        String output = "Test Name: "+this.title+"<br>" +"Cost: "+this.cost+"<br>"+"Availability: "+this.isAvailable;
+        return output;
+    }
+    
+   void search(String title){
+       if (this.title == title)
+           show();
+       else
+           System.out.println("Not Found");
+       
+   } 
+}
